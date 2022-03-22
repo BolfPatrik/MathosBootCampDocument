96c68f792db67f23c994ea40012b2bd743d14c2e
Patrik Bolf <bolfpatrik0@gmail.com>
2022-03-22 22:50:58 +0100

    CRUD Methods

---
+++ TestnaAplikacija/Test.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test.WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            {
+                IList<string> formatters = new List<string>();
+
+                foreach (var item in GlobalConfiguration.Configuration.Formatters)
+                {
+                    formatters.Add(item.ToString());
+                }
+
+                return formatters.AsEnumerable<string>();
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        // POST api/values
+        public string Post(Animal animal)
+        {
+            return animal.Name + ": " + animal.Species;
+        }
+        [HttpPut]
+        [Route("update")]
+        // PUT api/values/5
+        public void UpdateAnimal(int id, [FromBody] string value)
+        {
+        }
+        [HttpDelete]
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+
+    }
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ TestnaAplikacija/Test.WebApi/Models/Buyer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test.WebApi.Models
+{
+    public class Buyer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Oib { get; set; }
+    }
+}
